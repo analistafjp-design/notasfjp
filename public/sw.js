5fa29484dcb725b97c616adae50767327d736aa4
analistafjp-design <analistafjp@gmail.com>
2026-09-07 13:40:50 -0300

    Adicionar limpeza do aplicativo offline antigo

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter((key) => key.startsWith("fjp-flow-")).map((key) => caches.delete(key)));
+    await self.registration.unregister();
+    const clientsList = await self.clients.matchAll({ type: "window" });
+    for (const client of clientsList) client.navigate(client.url);
+  })());
+});
